a4aa7fb5405c6d909049c6a2e912b818e6fec4fd
Edwin Xie <exie@pivotal.io>
2016-06-07 12:25:02 -0700

    Make scripts/deploy better
    - searches for the directory that contains cf-release
    
    Signed-off-by: Shash Reddy <sreddy@pivotal.io>

--- scripts/get_paths.sh
+++ scripts/get_paths.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 absolute_path() {
   (cd $1 && pwd)
 }
@@ -7,8 +9,23 @@ absolute_path() {
 scripts_path=$(absolute_path `dirname $0`)
 
 ROUTING_RELEASE_DIR=${ROUTING_RELEASE_DIR:-$(absolute_path $scripts_path/..)}
-DIEGO_RELEASE_DIR=${DIEGO_RELEASE_DIR:-$(absolute_path $ROUTING_RELEASE_DIR/../diego-release)}
-CF_RELEASE_DIR=${CF_RELEASE_DIR:-$(absolute_path $ROUTING_RELEASE_DIR/../cf-release)}
+
+ROOT_DIR=$(
+  for i in `seq 1 5`; do
+    ls "cf-release" >> "/dev/null"
+    if [[ $? -ne 0 ]]; then  >> "/dev/null"
+      cd ..
+    else
+      break
+    fi
+  done
+  pwd
+)
+
+echo $ROOT_DIR
+
+DIEGO_RELEASE_DIR=${DIEGO_RELEASE_DIR:-$ROOT_DIR/diego-release}
+CF_RELEASE_DIR=${CF_RELEASE_DIR:-$ROOT_DIR/cf-release}
 
 ROUTING_MANIFESTS_DIR=$ROUTING_RELEASE_DIR/bosh-lite/deployments
 CF_MANIFESTS_DIR=$CF_RELEASE_DIR/bosh-lite/deployments
