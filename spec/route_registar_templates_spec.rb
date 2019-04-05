d5838eb16249827e8863a36eb2c7d8b0ed5d99ed
Joseph Palermo <jpalermo@pivotal.io>
2019-04-05 09:57:46 -0700

    Require `server_cert_domain_san` if `tls_port` on.
    For Route Registrar job
    
    [finishes #165034794](https://www.pivotaltracker.com/story/show/165034794)
    
    Closes #146
    
    Signed-off-by: Rodolfo Sanchez <rsanchez@pivotal.io>

---
+++ spec/route_registar_templates_spec.rb
@@ -0,0 +1,114 @@
+# rubocop: disable LineLength
+# rubocop: disable BlockLength
+require 'rspec'
+require 'bosh/template/test'
+require 'yaml'
+require 'json'
+
+module Bosh::Template::Test
+  describe 'route_registrar' do
+    let(:release_path) { File.join(File.dirname(__FILE__), '..') }
+    let(:release) { ReleaseDir.new(release_path) }
+    let(:job) { release.job('route_registrar') }
+
+    let(:merged_manifest_properties) do
+      {
+        'route_registrar' => {
+          'routes' => [
+            {
+              'health_check' => {
+                'name' => 'uaa-healthcheck',
+                'script_path' => '/var/vcap/jobs/uaa/bin/health_check'
+              },
+              'name' => 'uaa',
+              'registration_interval' => '10s',
+              'tags' => {
+                'component' => 'uaa'
+              },
+              'tls_port' => 8443,   #enables tls
+              'server_cert_domain_san' => 'valid_cert',
+              'uris' => [
+                'uaa.uaa-acceptance.cf-app.com',
+                '*.login.uaa-acceptance.cf-app.com'
+              ]
+            }
+          ]
+        }
+      }
+    end
+
+    describe 'config/registrar_settings.json' do
+      let(:template) { job.template('config/registrar_settings.json') }
+      let(:links) {
+        [
+          Link.new(
+            name: 'nats',
+            properties: {
+              'nats' => {
+                'host' => '', 'user' => '', 'password' => '', 'port' => 8080
+              }
+            }
+          )
+        ]
+      }
+
+      describe 'when given a valid set of properties' do
+        it 'renders the template' do
+          rendered_hash = JSON.load(template.render(merged_manifest_properties, consumes: links))
+          expect(rendered_hash).to eq({
+            'host' => '192.168.0.0',
+            'message_bus_servers' => [],
+            'routes' => [
+              {
+                'health_check'=> {'name'=>'uaa-healthcheck', 'script_path'=>'/var/vcap/jobs/uaa/bin/health_check'},
+                'name'=>'uaa',
+                'registration_interval'=>'10s',
+                'tags'=>{'component'=>'uaa'},
+                'tls_port'=>8443,
+                'server_cert_domain_san'=>'valid_cert',
+                'uris'=>['uaa.uaa-acceptance.cf-app.com', '*.login.uaa-acceptance.cf-app.com']
+              }
+            ],
+            'routing_api' => {
+              'ca_certs'=>'/var/vcap/jobs/route_registrar/config/certs/ca.crt',
+              'api_url'=>'http://routing-api.service.cf.internal:3000',
+              'oauth_url'=>'https://uaa.service.cf.internal:8443',
+              'client_id'=>'routing_api_client'
+            }
+          })
+        end
+      end
+
+      describe 'when tls is enabled and the san is not provided' do
+        before do
+          merged_manifest_properties['route_registrar']['routes'][0].delete('server_cert_domain_san')
+        end
+        it 'should required san if tls_port is provided' do
+          expect { template.render(merged_manifest_properties, consumes: links) }.to raise_error(
+            RuntimeError, 'expected route_registrar.routes[0].route.server_cert_domain_san when tls_port is provided')
+        end
+      end
+
+      describe 'when tls is enabled and the san is not provided' do
+        before do
+          merged_manifest_properties['route_registrar']['routes'][0]['server_cert_domain_san'] = ''
+        end
+        it 'should required san if tls_port is provided' do
+          expect { template.render(merged_manifest_properties, consumes: links) }.to raise_error(
+            RuntimeError, 'expected route_registrar.routes[0].route.server_cert_domain_san when tls_port is provided')
+        end
+      end
+
+      describe 'when tls is not enabled and the san is not provided' do
+        before do
+          merged_manifest_properties['route_registrar']['routes'][0].delete('tls_port')
+          merged_manifest_properties['route_registrar']['routes'][0].delete('server_cert_domain_san')
+        end
+
+        it 'renders the template' do
+          expect { template.render(merged_manifest_properties, consumes: links) }.not_to raise_error(RuntimeError)
+        end
+      end
+    end
+  end
+end
