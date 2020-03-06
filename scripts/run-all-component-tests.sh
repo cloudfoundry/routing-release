10a15fe6061618122b7f88553982358860fcebdd
Clay Kauzlaric <ckauzlaric@pivotal.io>
2020-03-05 16:14:58 -0800

    ci: add flakeattempts flag to unit tests
    * ci will now make three attempts to run each test in unit tests
    
    Bump src/code.cloudfoundry.org/gorouter
       test: remove retries
       ci: add global timeout to tests
    
    Co-authored-by: Bruce Ricard <bricard@pivotal.io>

--- scripts/run-all-component-tests.sh
+++ scripts/run-all-component-tests.sh
@@ -7,13 +7,13 @@ PACKAGE="$1"
 if [[ -n "${PACKAGE}" ]]; then
   pushd "./src/code.cloudfoundry.org/${PACKAGE}"
     echo "Testing component: ${PACKAGE}"
-    ./bin/test
+    ./bin/test -flakeAttempts 3
   popd
 else
   for component in gorouter cf-tcp-router multierror route-registrar routing-api routing-api-cli uaa-go-client; do
     pushd src/code.cloudfoundry.org/${component}
       echo "Testing component: ${component}..."
-      ./bin/test
+      ./bin/test -flakeAttempts 3
     popd
   done
 fi
