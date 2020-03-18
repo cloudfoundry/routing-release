9ff93a6d2a6b00f0314b78c15e5568a7e58be935
Amelia Downs <adowns@pivotal.io>
2020-03-18 12:09:58 -0700

    feat: support rfc3339 for pre-start/post-start
    This spec property is not fully implemented yet.
    We still need to support `deprecated` for gorouter code.
    
    [#171242274](https://www.pivotaltracker.com/story/show/171242274)
    
    Co-authored-by: Aidan Obley <aobley@vmware.com>

--- src/routing_utils/syslog_utils.sh
+++ src/routing_utils/syslog_utils.sh
@@ -6,11 +6,21 @@
 function tee_output_to_sys_log() {
   declare log_dir="$1"
   declare log_name="$2"
+  declare log_format="$3"
 
-  exec > >(tee -a >(logger -p user.info -t "vcap.${log_name}.stdout") | prepend_datetime >>"${log_dir}/${log_name}.log")
-  exec 2> >(tee -a >(logger -p user.error -t "vcap.${log_name}.stderr") | prepend_datetime >>"${log_dir}/${log_name}.err.log")
+  if [ "$3" == "deprecated" ]; then
+    exec > >(tee -a >(logger -p user.info -t "vcap.${log_name}.stdout") | prepend_datetime >>"${log_dir}/${log_name}.log")
+-   exec 2> >(tee -a >(logger -p user.error -t "vcap.${log_name}.stderr") | prepend_datetime >>"${log_dir}/${log_name}.err.log")
+  else
+    exec > >(tee -a >(logger -p user.info -t "vcap.${log_name}.stdout") | prepend_rfc3339_datetime >>"${log_dir}/${log_name}.log")
+    exec 2> >(tee -a >(logger -p user.error -t "vcap.${log_name}.stderr") | prepend_rfc3339_datetime >>"${log_dir}/${log_name}.err.log")
+  fi
 }
 
 function prepend_datetime() {
   perl -ne 'BEGIN { use Time::HiRes "time"; use POSIX "strftime"; STDOUT->autoflush(1) }; my $t = time; my $fsec = sprintf ".%06d", ($t-int($t))*1000000; my $time = strftime("[%Y-%m-%d %H:%M:%S".$fsec."%z]", localtime $t); print("$time $_")'
+}
+
+function prepend_rfc3339_datetime() {
+  perl -ne 'BEGIN { use Time::HiRes "time"; use POSIX "strftime"; STDOUT->autoflush(1) }; my $t = time; my $fsec = sprintf ".%09d", ($t-int($t))*1000000000; my $time = strftime("[%Y-%m-%dT%H:%M:%S".$fsec."Z]", localtime $t); print("$time $_")'
 }
