669df5419bf74f2f33ac91ef3e81601c2e2483ad
Marc Paquette <mpaquette@vmware.com>
2021-12-17 17:47:49 +0000

    wip: modify test scripts / envrc
    Signed-off-by: Josh Russett <jrussett@vmware.com>

--- scripts/start-docker-for-testing.sh
+++ scripts/start-docker-for-testing.sh
@@ -3,11 +3,11 @@
 BASEDIR="$(cd $(dirname "$0") && pwd)"
 ROUTING_RELEASE_DIR="${BASEDIR}/.."
 
-IMAGE="cloudfoundry/cf-routing-pipeline"
+IMAGE="cf-routing-pipeline_go1.17.4"
 MOUNT_DIR="/routing-release"
 PREPARE_CONTAINER="cd ${MOUNT_DIR}; source .envrc; scripts/setup-test-environment.sh; bash"
 
-docker pull "${IMAGE}"
+# docker pull "${IMAGE}"
 docker run -it \
   -v "${ROUTING_RELEASE_DIR}:${MOUNT_DIR}" \
   "${IMAGE}" \
