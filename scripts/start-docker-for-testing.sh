c26e8c567cf3747973bb000ed4f7461589255e06
Josh Russett <jrussett@vmware.com>
2022-01-05 22:34:39 +0000

    Corrects scripts/start-docker-for-testing

--- scripts/start-docker-for-testing.sh
+++ scripts/start-docker-for-testing.sh
@@ -3,11 +3,11 @@
 BASEDIR="$(cd $(dirname "$0") && pwd)"
 ROUTING_RELEASE_DIR="${BASEDIR}/.."
 
-IMAGE="cf-routing-pipeline_go1.17.4"
+IMAGE="cf-routing-pipeline"
 MOUNT_DIR="/routing-release"
 PREPARE_CONTAINER="cd ${MOUNT_DIR}; source .envrc; scripts/setup-test-environment.sh; bash"
 
-# docker pull "${IMAGE}"
+docker pull "${IMAGE}"
 docker run -it \
   -v "${ROUTING_RELEASE_DIR}:${MOUNT_DIR}" \
   "${IMAGE}" \
