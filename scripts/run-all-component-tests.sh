5f733a05b0afbcad4e5aaf97831b618201b99185
Amelia Downs <adowns@vmware.com>
2022-03-10 17:41:26 +0000

    Allow test scripts to specify components to test vs everything
    Signed-off-by: Geoff Franks <gfranks@vmware.com>

--- scripts/run-all-component-tests.sh
+++ scripts/run-all-component-tests.sh
@@ -3,11 +3,16 @@
 set -e
 
 PACKAGE="$1"
+SUB_PACKAGE="$2"
 
 if [[ -n "${PACKAGE}" ]]; then
   pushd "./src/code.cloudfoundry.org/${PACKAGE}"
     echo "Testing component: ${PACKAGE}"
-    ./bin/test -flakeAttempts 3
+    if [[ -n "${SUB_PACKAGE}" ]]; then
+      ./bin/test -flakeAttempts 3 ${SUB_PACKAGE}
+    else
+      ./bin/test -flakeAttempts 3
+    fi
   popd
 else
   for component in gorouter cf-tcp-router multierror route-registrar routing-api routing-api-cli uaa-go-client; do
