87326beac5abd4ac82367fcbea4484b4cd327fd1
Maria Shaldybin <mariash@vmware.com>
2022-08-03 20:33:32 +0000

    Update format for kill command

--- scripts/setup-test-environment.sh
+++ scripts/setup-test-environment.sh
@@ -11,7 +11,7 @@ chown -R mysql:mysql /var/lib/mysql /var/log/mysql /var/run/mysqld
 chown -R postgres:postgres /var/lib/postgresql /var/log/postgresql /var/run/postgresql /etc/postgresql
 chown -R root:ssl-cert /etc/ssl/private
 
-kill -HUP $(pidof rsyslogd)
+kill -s HUP $(pidof rsyslogd)
 service mariadb restart
 service postgresql restart
 
