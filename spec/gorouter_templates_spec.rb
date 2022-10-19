a7c0ddb6dffb96740113c6472994e26cefa98967
Amelia Downs <adowns@vmware.com>
2022-10-19 14:12:41 -0400

    Reserve important ports in ephemeral range (#293)
    * remove important ports from ephemeral range
    * update ip_local_reserved_ports spec description
    
    Signed-off-by: Josh Russett <jrussett@vmware.com>
    Co-authored-by: Amelia Downs <adowns@vmware.com>

--- spec/gorouter_templates_spec.rb
+++ spec/gorouter_templates_spec.rb
@@ -1161,6 +1161,45 @@ describe 'gorouter' do
       end
     end
   end
+
+  describe 'pre-start' do
+    let(:template) { job.template('bin/pre-start') }
+    let(:properties) do
+      { 'router' => {
+        'port' => 81,
+        'status' => { 'port' => 8081 },
+        'prometheus' => { 'port' => 7070 },
+        'tls_port' => 442,
+        'debug_address' => '127.0.0.1:17003'
+      } }
+    end
+
+    context 'ip_local_reserved_ports' do
+      it 'contains reserved ports in order' do
+        rendered_template = template.render(properties)
+        ports = '81,442,2822,2825,3458,3459,3460,3461,7070,8081,8853,17003,53080'
+        expect(rendered_template).to include("\"#{ports}\" > /proc/sys/net/ipv4/ip_local_reserved_ports")
+      end
+
+      context 'when prometheus port is not set' do
+        it 'skips that port' do
+          properties['router'].delete('prometheus')
+          rendered_template = template.render(properties)
+          ports = '81,442,2822,2825,3458,3459,3460,3461,8081,8853,17003,53080'
+          expect(rendered_template).to include("\"#{ports}\" > /proc/sys/net/ipv4/ip_local_reserved_ports")
+        end
+      end
+
+      context 'when debug_address does not contain a port' do
+        it 'skips that port' do
+          properties['router']['debug_address'] = 'meow'
+          rendered_template = template.render(properties)
+          ports = '81,442,2822,2825,3458,3459,3460,3461,7070,8081,8853,53080'
+          expect(rendered_template).to include("\"#{ports}\" > /proc/sys/net/ipv4/ip_local_reserved_ports")
+        end
+      end
+    end
+  end
 end
 
 # rubocop:enable Layout/LineLength
