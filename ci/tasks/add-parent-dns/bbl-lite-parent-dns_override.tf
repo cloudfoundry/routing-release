a36be4bee05335859826bec866c56d0cac0b53b1
Josh Russett <jrussett@vmware.com>
2023-03-17 22:25:30 +0000

    ci: remove unused ci tasks
    I don't think these are used anywhere. I decided to clean these up as a
    part of the switch from using zero-downtime-release to uptimer.
    
    Tangentially related to:
    [#183976643](https://www.pivotaltracker.com/story/show/183976643)

--- ci/tasks/add-parent-dns/bbl-lite-parent-dns_override.tf
+++
@@ -1,9 +0,0 @@
-resource "google_dns_record_set" "parent_dns_pointer" {
-  name = "*.${var.env_id}.routing.cf-app.com."
-  type = "A"
-  ttl  = 300
-
-  managed_zone = "routing-team"
-
-  rrdatas = ["${google_compute_address.bosh-director-ip.address}"]
-}
