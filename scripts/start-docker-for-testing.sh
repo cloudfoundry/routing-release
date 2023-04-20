664966335d2425576ee2d4148300687683fc8557
Geoff Franks <gfranks@vmware.com>
2023-04-20 16:38:37 +0000

    Revert "Bump haproxy to 2.7.6 (#318)"
    This reverts commit ba5e3ba274b026f7cfd8936061d5ce7917783dd3.

--- scripts/start-docker-for-testing.sh
+++ scripts/start-docker-for-testing.sh
@@ -10,7 +10,6 @@ PREPARE_CONTAINER="cd ${MOUNT_DIR}; source .envrc; scripts/setup-test-environmen
 docker pull "${IMAGE}"
 docker run -it \
   -v "${ROUTING_RELEASE_DIR}:${MOUNT_DIR}" \
-  -e "GOFLAGS=-buildvcs=false" \
   "${IMAGE}" \
   /bin/bash -c "${PREPARE_CONTAINER}"
 
