bea4f79623385902564e855f477b64179a98a997
Josh Russett <jrussett@vmware.com>
2023-05-04 22:31:22 +0000

    Use stndrd imgs in template/unit/integration tests
    Modifies the scripts to be able to make use of the standard images:
    - cloudfoundry/tas-runtime-build
    - cloudfoundry/tas-runtime-mysql-5.7
    - cloudfoundry/tas-runtime-mysql-8.0
    - cloudfoundry/tas-runtime-postgres
    
    Submodule src/code.cloudfoundry.org/cf-tcp-router 6b149c7f..ecebe81f:
      > Genericize locket setup for postgres too (#13)
    Submodule src/code.cloudfoundry.org/routing-api 2bf56685..629f7a89:
      > Genericize test setup for postgres too (#32)
    
    The bumps to `cf-tcp-router` and `routing-api` modify the tests to run
    with only one of the sql databases, instead of the assumed two of both
    mysql and postgres present in the old image. Now when the
    units/integration tests run, all of the components will make use of
    mysql when running in a mysql container, and vice versa for postgres.
    
    This also breaks the template-tests out of being run as part of the unit
    tests. We already had a script to only run the template-tests:
    `scripts/template-tests`, which is easily executed from a docker
    container in the other script `scripts/run-template-tests-in-docker`.
    
    [#182691815](https://www.pivotaltracker.com/story/show/182691815)

--- scripts/setup-test-environment.sh
+++ scripts/setup-test-environment.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 set -ex -o pipefail
 
-bin_dir="$(dirname "$0")/../bin"
-tmpdir=$(mktemp -d /tmp/cf-routing-downloads.XXXX)
+function bootDB {
+  db="$1"
+
+  if [ "${db}" = "postgres" ]; then
+    launchDB="(/postgres-entrypoint.sh postgres &> /var/log/postgres-boot.log) &"
+    testConnection="psql -h localhost -U postgres -c '\conninfo'"
+  elif [ "${db}" = "mysql" ]  || [ "${db}" = "mysql-5.6" ] || [ "${db}" = "mysql8" ]; then
+    launchDB="(MYSQL_ROOT_PASSWORD=password /mysql-entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
+    testConnection="mysql -h localhost -u root -D mysql -e '\s;' --password='password'"
+  else
+    echo "DB variable not set. The script does not determine which database to use and would fail some tests with errors related to being unable to connect to the db. Bailing early."
+    exit 1
+  fi
+
+  echo -n "booting ${db}"
+  eval "$launchDB"
+  for _ in $(seq 1 60); do
+    if eval "${testConnection}" &> /dev/null; then
+      echo "connection established to ${db}"
+      return 0
+    fi
+    echo -n "."
+    sleep 1
+  done
+  eval "${testConnection}" || true
+  echo "unable to connect to ${db}"
+  exit 1
+}
 
 echo "Some Gorouter tests require the machine to be Linux and not anything else like Windows or Mac"
 
-# setup mysql and postgres
-chown -R mysql:mysql /var/lib/mysql /var/log/mysql /var/run/mysqld
-chown -R postgres:postgres /var/lib/postgresql /var/log/postgresql /var/run/postgresql /etc/postgresql
-chown -R root:ssl-cert /etc/ssl/private
-
 rsyslog_pid=$(pidof rsyslogd || true)
-if [[ -z $rsyslog_pid ]]; then
+if [[ -z "${rsyslog_pid}" ]]; then
   rsyslogd
 else
-  kill -HUP $(pidof rsyslogd)
+  kill -HUP "$(pidof rsyslogd)"
 fi
-service mariadb restart
-service postgresql restart
 
-echo "Running template tests"
-  # gem install bundler
-  bundle install
-  rubocop spec # fix these errors by running 'rubocop -a spec' or add an ignore directive
-  bundle exec rspec
-echo "Finished running template tests"
-
-export BIN_DIR="$PWD/bin"
-export PATH=$PATH:$BIN_DIR
+export BIN_DIR="${PWD}/bin"
+export PATH="${PATH}:${BIN_DIR}"
 
 pushd src/code.cloudfoundry.org
-go build -o "$BIN_DIR/nats-server" github.com/nats-io/nats-server/v2
-go build -o "$BIN_DIR/ginkgo" github.com/onsi/ginkgo/v2/ginkgo
+go build -o "${BIN_DIR}/nats-server" github.com/nats-io/nats-server/v2
+go build -o "${BIN_DIR}/ginkgo" github.com/onsi/ginkgo/v2/ginkgo
 popd
 
+bootDB "${DB}"
 echo "Done setting up for tests"
--- scripts/start-docker-for-testing.sh
+++ scripts/start-docker-for-testing.sh
@@ -3,9 +3,10 @@
 BASEDIR="$(cd $(dirname "$0") && pwd)"
 ROUTING_RELEASE_DIR="${BASEDIR}/.."
 
-IMAGE="cloudfoundry/cf-routing-pipeline"
+IMAGE="cloudfoundry/tas-runtime-mysql-8.0"
+DB="mysql"
 MOUNT_DIR="/routing-release"
-PREPARE_CONTAINER="cd ${MOUNT_DIR}; source .envrc; scripts/setup-test-environment.sh; bash"
+PREPARE_CONTAINER="export DB=${DB}; cd ${MOUNT_DIR}; source .envrc; scripts/setup-test-environment.sh; bash"
 
 docker pull "${IMAGE}"
 docker run -it \
