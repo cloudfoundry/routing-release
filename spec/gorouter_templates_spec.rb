5326d042b23624337c2767e0f1f2442e4bbfc9cd
Mike Yeromko <mykhailo.yeromko@sap.com>
2025-02-19 08:26:28 +0100

    feat: add histogram buckets configuration

--- spec/gorouter_templates_spec.rb
+++ spec/gorouter_templates_spec.rb
@@ -730,6 +730,24 @@ describe 'gorouter' do
             expect(parsed_yaml['prometheus']['ca_path']).to eq('/var/vcap/jobs/gorouter/config/certs/prometheus/prometheus_ca.crt')
           end
         end
+        context 'when prometheus meters are configured' do
+          before do
+            deployment_manifest_fragment['router']['per_app_prometheus_http_metrics_reporting'] = true
+            deployment_manifest_fragment['router']['prometheus'] = {
+              'port' => 9090,
+              'meters' => {
+                'route_lookup_time_histogram_buckets' => [0, 100, 10000],
+                'route_registration_latency_histogram_buckets' => [-10, 0, 10],
+                'routing_response_latency_histogram_buckets' => [0.1, 0.5, 1]
+              }
+            }
+          end
+          it 'should set prometheus meters configuration' do
+            expect(parsed_yaml['prometheus']['meters']['route_lookup_time_histogram_buckets']).to eq([0, 100, 10000])
+            expect(parsed_yaml['prometheus']['meters']['route_registration_latency_histogram_buckets']).to eq([-10, 0, 10])
+            expect(parsed_yaml['prometheus']['meters']['routing_response_latency_histogram_buckets']).to eq([0.1, 0.5, 1])
+          end
+        end
       end
 
       describe 'route_services' do
