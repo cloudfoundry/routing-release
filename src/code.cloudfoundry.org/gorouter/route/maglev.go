010c4e0b762e11b347ae2149f6495527457f669f
Clemens Hoffmann <clemens.hoffmann@sap.com>
2025-10-29 14:32:37 +0100

    Add LICENSE information for maglev.go

--- src/code.cloudfoundry.org/gorouter/route/maglev.go
+++ src/code.cloudfoundry.org/gorouter/route/maglev.go
@@ -1,5 +1,15 @@
 package route
 
+/******************************************************************************
+ * Original github.com/kkdai/maglev/maglev.go
+ *
+ * Copyright (c) 2019 Evan Lin (github.com/kkdai)
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Apache License, Version 2.0 which is available at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ ******************************************************************************/
+
 import (
 	"errors"
 	"fmt"
