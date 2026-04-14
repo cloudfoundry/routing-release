e8810a7e217ed317d7d072e2ed39718075c78426
Clemens Hoffmann <clemens.hoffmann@sap.com>
2026-04-14 15:51:50 +0200

    Use global defaultEventuallyTimeout 10s

--- src/code.cloudfoundry.org/gorouter/integration/init_test.go
+++ src/code.cloudfoundry.org/gorouter/integration/init_test.go
@@ -48,7 +48,7 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	json.Unmarshal(data, &res)
 	gorouterPath = res.Gorouter
 	testAssets = res.Test
-	SetDefaultEventuallyTimeout(5 * time.Second)
+	SetDefaultEventuallyTimeout(10 * time.Second)
 	SetDefaultEventuallyPollingInterval(100 * time.Millisecond)
 	SetDefaultConsistentlyDuration(1 * time.Second)
 	SetDefaultConsistentlyPollingInterval(10 * time.Millisecond)
